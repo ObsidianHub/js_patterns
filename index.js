435154c4f45301f62bce989bf73f4b4901979bea
Denny Pearce <obsidian.y.dragon@gmail.com>
2019-12-13 01:33:28 +0200

    add pattern - observer

--- index.js
+++ index.js
@@ -76,3 +76,40 @@ const factory = new MemberFactory();
 members.push(factory.createMember('Denis', 'standart'));
 members.push(factory.createMember('Katya', 'premium'));
 
+// Observer pattern
+class EventObserver {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(fn) {
+        this.observers.push(fn);
+    }
+
+    unsubsribe(fn) {
+        this.observers = this.observers.filter((item) => {
+            if (item !== fn) {
+                return item;
+            }
+        });
+    }
+
+    fire(args) {
+        this.observers.forEach((fn) => {
+            fn.call(null, args);
+        });
+    }
+}
+
+const addNewTask = new EventObserver();
+
+function alertMessage(msg) {
+    alert(msg);
+}
+
+function consoleMessage(msg) {
+    console.log(msg);
+}
+
+addNewTask.subscribe(alertMessage);
+addNewTask.subscribe(consoleMessage);
