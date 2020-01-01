9a7474fd8037d2297fc21a92e77f5131e214ad12
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-01 20:47:09 +0200

    delete

--- index.js
+++ index.js
@@ -27,55 +27,6 @@ const counterModule = (function() {
 
 const counter1 = counterModule.getInstance();
 
-// Factory pattern
-class SimpleMemberShip {
-    constructor(name) {
-        this.name = name;
-        this.cost = '$5';
-    }
-}
-
-class StandartMemberShip {
-    constructor(name) {
-        this.name = name;
-        this.cost = '$15';
-    }
-}
-
-class PremiumMemberShip {
-    constructor(name) {
-        this.name = name;
-        this.cost = '$25';
-    }
-}
-
-function MemberFactory() {
-    this.createMember = function(name, type) {
-        let member;
-
-        if (type === 'simple') {
-            member = new SimpleMemberShip(name);
-        } else if (type === 'standart') {
-            member = new StandartMemberShip(name);
-        } else if (type === 'premium') {
-            member = new PremiumMemberShip(name);
-        }
-
-        member.type = type;
-        member.define = function() {
-            return `${this.name} (${this.type}): ${this.cost}`;
-        }
-
-        return member;
-    }
-}
-
-const members = [];
-const factory = new MemberFactory();
-
-members.push(factory.createMember('Denis', 'standart'));
-members.push(factory.createMember('Katya', 'premium'));
-
 // Observer pattern
 class EventObserver {
     constructor() {
